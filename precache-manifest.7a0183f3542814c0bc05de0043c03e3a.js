d6e993dfffd5f35c6c120f082038f3baa87d6ea0
Linead96 <danielgarboza@yahoo.com>
2019-03-12 15:30:43 +0800

    Updates

---
+++ precache-manifest.7a0183f3542814c0bc05de0043c03e3a.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "8aa5004aac550df68369b7210809fe57",
+    "url": "/linead96/my-portfolio-in-react.git/static/media/about-me-bg.8aa5004a.png"
+  },
+  {
+    "revision": "0c132d5e2bf77c884f4b",
+    "url": "/linead96/my-portfolio-in-react.git/static/css/main.70059ac1.chunk.css"
+  },
+  {
+    "revision": "47802a365b3e6df445b4",
+    "url": "/linead96/my-portfolio-in-react.git/static/js/runtime~main.a2b01998.js"
+  },
+  {
+    "revision": "e56553757385be31cd65",
+    "url": "/linead96/my-portfolio-in-react.git/static/js/2.1226e321.chunk.js"
+  },
+  {
+    "revision": "9c05370f732a96b3b38171a861f54082",
+    "url": "/linead96/my-portfolio-in-react.git/static/media/my-projects-bg.9c05370f.png"
+  },
+  {
+    "revision": "710870f7c7cc64416ef7e00e0d952627",
+    "url": "/linead96/my-portfolio-in-react.git/static/media/body-navbar-background.710870f7.png"
+  },
+  {
+    "revision": "0c132d5e2bf77c884f4b",
+    "url": "/linead96/my-portfolio-in-react.git/static/js/main.fe70fc14.chunk.js"
+  },
+  {
+    "revision": "c250bf68bf746f679189d26d861bb412",
+    "url": "/linead96/my-portfolio-in-react.git/static/media/header-background.c250bf68.png"
+  },
+  {
+    "revision": "f802ab74b83649e9d7892ea76af0351c",
+    "url": "/linead96/my-portfolio-in-react.git/static/media/my-project-buod.f802ab74.png"
+  },
+  {
+    "revision": "d497d1222fc6e307efce381e334499e1",
+    "url": "/linead96/my-portfolio-in-react.git/static/media/my-project-elyza.d497d122.png"
+  },
+  {
+    "revision": "7a582e97b9d2895561b1caac2e5b2c5d",
+    "url": "/linead96/my-portfolio-in-react.git/static/media/my-project-yelpcamp.7a582e97.png"
+  },
+  {
+    "revision": "dcf0afd1cc4f8791da8b053a3981eab5",
+    "url": "/linead96/my-portfolio-in-react.git/static/media/my-project-grid.dcf0afd1.png"
+  },
+  {
+    "revision": "746fd98d69a01800767ab0687c0b1ad6",
+    "url": "/linead96/my-portfolio-in-react.git/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/linead96/my-portfolio-in-react.git/precache-manifest.7a0183f3542814c0bc05de0043c03e3a.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/linead96/my-portfolio-in-react.git/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
